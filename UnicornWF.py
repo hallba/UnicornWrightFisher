fd28853499ca9ad93d86e845bf322ae42ebffc54
Ben Hall <b.hall@ucl.ac.uk>
2021-03-02 19:35:56 +0000

    Unicorn projection

---
+++ UnicornWF.py
@@ -0,0 +1,27 @@
+import unicornhat as unicorn
+import WrightFisher
+from time import sleep
+
+unicorn.set_layout(unicorn.AUTO)
+unicorn.rotation(0)
+unicorn.brightness(0.5)
+width,height=unicorn.get_shape()
+
+colourMap = {   0: (0, 0, 0), 
+                1: (255, 0, 0),
+                2: (0, 255, 0),
+                3: (0, 0, 255) }
+
+class UnicornSimulator(WrightFisher.Simulator):
+    def project(self):
+        for i in range(self.size):
+            for j in range(self.size):
+                index =i+j*self.size
+                colour = colourMap[self.colour[index]]
+                unicorn.set_pixel(i,j,colour[0],colour[1],colour[2])
+        unicorn.show()
+    def runAndProject(self,steps):
+        for _ in range(steps):
+            self.update()
+            self.project()
+            sleep(self.wait)
