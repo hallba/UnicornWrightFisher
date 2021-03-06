5c05f6a87a1e504e1c1c5034da40d897200847f1
Ben Hall <b.hall@ucl.ac.uk>
2021-03-06 23:06:25 +0000

    Reimplementation for pico

---
+++ picoCloneWars.py
@@ -0,0 +1,159 @@
+"""Wright Fisher simulator.
+
+Simulates the growth of clones in a 2D space.
+"""
+
+from time import sleep
+
+import random
+
+def deepcopy(someArray):
+    return([someArray[i] for i in range(len(someArray))])
+
+from machine import Pin, Timer
+led = Pin(25, Pin.OUT)
+timer = Timer()
+
+import picounicorn as uni
+
+uni.init()
+w = uni.get_width()
+h = uni.get_height()
+#print('Unicorn size',w,'by',h)
+#uni.set_pixel(x,y, r, g, b)
+
+class Simulator:
+    """Simulator for 2D Wright Fisher.
+
+    Simulates and visualises a 2D square.
+    """
+
+    def __init__(self, size, mutationRate=0, wait=1, numberColours=4, advantage=0):
+        """Initialise."""
+        # Generate a grid of black cells initially
+        self.size = size
+        self.population = size * size
+        self.fitness = [0. for _ in range(self.population)]
+        self.colour = [0 for _ in range(self.population)]
+
+        self.nextFitness = [0. for _ in range(self.population)]
+        self.nextColour = [0 for _ in range(self.population)]
+
+        self.mutationRate = mutationRate
+        self.timer = 0
+        self.advantage = advantage
+
+        self.mutantColour = 1
+        self.totalColours = numberColours
+
+        self.wait = wait
+
+        self.specificInit()
+        """
+                1
+            4   0   2
+                3
+        """
+    def specificInit(self):
+        pass
+    def up(self, currentPosition):
+        """Return array index of space above currentPosition."""
+        if currentPosition < self.size:
+            return currentPosition + self.size * (self.size - 1)
+        else:
+            return currentPosition - self.size
+
+    def down(self, currentPosition):
+        """Return array index of space below currentPosition."""
+        if currentPosition >= (self.size * (self.size - 1)):
+            return currentPosition - self.size * (self.size - 1)
+        else:
+            return currentPosition + self.size
+
+    def right(self, currentPosition):
+        """Return array index of space to right of currentPosition."""
+        if currentPosition % (self.size - 1) == 0:
+            return currentPosition - self.size + 1
+        else:
+            return currentPosition + 1
+
+    def left(self, currentPosition):
+        """Return array index of space to left of currentPosition."""
+        if currentPosition % self.size == 0:
+            return currentPosition + self.size - 1
+        else:
+            return currentPosition - 1
+
+    def replacement(self, source, target):
+        """Decide whether to replace cell and do it."""
+        probability = 0.5 - self.fitness[target] + self.fitness[source]
+        if random.random() < probability:
+            # replaced by upper cell
+            self.nextFitness[target] = self.fitness[source]
+            self.nextColour[target] = self.colour[source]
+        else:
+            self.nextColour[target] = self.colour[target]
+            self.nextFitness[target] = self.fitness[target]
+
+    def tryReplaceNeighbour(self, currentPosition):
+        """Pick a neighbour to try replace a cell and run replacement."""
+        which = random.randint(0, 6)
+        if which == 0:
+            # Do nothing as the cell is the same
+            self.nextColour[currentPosition] = self.colour[currentPosition]
+            self.nextFitness[currentPosition] = self.fitness[currentPosition]
+        elif which == 1:
+            self.replacement(self.up(currentPosition), currentPosition)
+        elif which == 2:
+            self.replacement(self.right(currentPosition), currentPosition)
+        elif which == 3:
+            self.replacement(self.down(currentPosition), currentPosition)
+        else:
+            self.replacement(self.left(currentPosition), currentPosition)
+
+    def colourUpdate(self):
+        """Change the colour to the next colour, wrapping if appropriate."""
+        self.mutantColour = (self.mutantColour + 1) % self.totalColours
+
+    def mutate(self, colour=None):
+        """Select a random cell and change fitness and colour."""
+        cell = random.randint(0, (self.population-1))
+        self.fitness[cell] += random.uniform((self.advantage-0.15), (self.advantage+0.15)) # originally normal
+        if colour == None:
+            self.colour[cell] = self.mutantColour
+        else:
+            self.colour[cell] = colour
+        self.colourUpdate()
+
+    def update(self):
+        """Update entire population."""
+        for cellIndex in range(self.population):
+            self.tryReplaceNeighbour(cellIndex)
+        self.fitness = deepcopy(self.nextFitness)
+        self.colour = deepcopy(self.nextColour)
+        mutating = self.mutationRate and self.timer
+        if mutating and self.timer % self.mutationRate == 0:
+            self.mutate()
+        self.timer += 1
+        led.toggle()
+        sleep(0.01)
+        led.toggle()
+
+    def printColours(self):
+        """Print colours to screen."""
+        for i in range(self.size):
+            for j in range(self.size):
+                print(self.colour[i + j * self.size], end="")
+            print("")
+
+    def runAndPrint(self, steps):
+        """Update for several steps, printing at each step."""
+        for _ in range(steps):
+            self.update()
+            self.printColours()
+            print("=" * self.size)
+            sleep(self.wait)
+
+grid = Simulator(5,2)
+grid.runAndPrint(100)
+
